07a2d459fa105d1b56652270ac62b2a6eedd6e77
Carlos <karlossanpa@gmail.com>
2018-10-04 22:36:17 +0200

    First version. Everything is a mandoline.

--- train.py
+++ train.py
@@ -114,7 +114,7 @@ def train(num_iteration):
             epoch = int(i / int(data.train.num_examples/batch_size))
 
             show_progress(epoch, feed_dict_tr, feed_dict_val, val_loss)
-            saver.save(session, './dogs-cats-model')
+            saver.save(session, './image_recognition')
 
 
     total_iterations += num_iteration
